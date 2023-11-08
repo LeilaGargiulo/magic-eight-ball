745cfca82a946553a2b146cd92b5eadb9a10a27f
LeilaGargiulo <144458769+LeilaGargiulo@users.noreply.github.com>
2023-11-08 15:14:30 +0000

    Nothing

---
+++ script.js
@@ -0,0 +1,30 @@
+let(1) = "Maybe"
+let(2) = "No" 
+let(3) = "yes"
+let(4) = "This happens in the near future"
+let(5) = "This happens in your far future"
+let(6) = "This will never occur"
+let(7) = "This will absolutely never occur"
+
+let(Math.floor(Math.random(1-7))) = rounded
+
+if (rounded = 1){
+    console.log(1)
+}
+ifelse (rounded = 2){
+    console.log(2);
+}
+ifelse (rounded = 3){
+    console.log(3);
+}
+ifelse (rounded = 4){
+    console.log(4);
+}
+ifelse(rounded = 5){
+    console.log(5);
+}
+ifelse(rounded = 6){
+    console.log(6);
+}else {
+    console.log(7);
+}
